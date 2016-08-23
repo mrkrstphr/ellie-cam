0ef78b2030a866365f62ebcd450144991f14f3e4
Kristopher Wilson <kristopherwilson@gmail.com>
2016-08-23 16:30:10 -0400

    Adding ability to save snapshots

--- app/src/main/java/com/grcodingcompany/elliecam/ViewFeed.java
+++ app/src/main/java/com/grcodingcompany/elliecam/ViewFeed.java
@@ -1,14 +1,27 @@
 package com.grcodingcompany.elliecam;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -86,18 +99,74 @@ public class ViewFeed extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
         }
 
+        if (id == R.id.save_photo) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 99);
+                return false;
+            }
+
+            saveSnapshot();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 99: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    saveSnapshot();
+                }
+            }
+        }
+    }
+
+    private void saveSnapshot() {
+        findViewById(R.id.imageView).buildDrawingCache();
+        Bitmap bm = findViewById(R.id.imageView).getDrawingCache();
+
+        try {
+            String photoFile = MediaStore.Images.Media.insertImage(getContentResolver(), bm, "EllieCam", "EllieCam Snapshot");
+            showSnapshotNotification(photoFile, bm);
+        } catch (Exception e) {
+            Toast.makeText(this, "Please try again later. " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showSnapshotNotification(String photoFile, Bitmap bm)
+    {
+        Intent intent = new Intent();
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(photoFile), "image/*");
+
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        Notification.Builder mBuilder =
+                new Notification.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(bm)
+                        .setContentTitle("EllieCam")
+                        .setContentText("Snapshot saved! Click to view")
+                        .setContentIntent(pIntent)
+                        .setStyle(new Notification.BigPictureStyle()
+                                .bigPicture(bm));
+
+        Notification notify = mBuilder.build();
+        notify.flags |= Notification.FLAG_AUTO_CANCEL | Notification.FLAG_SHOW_LIGHTS;
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(55, notify);
     }
 }
